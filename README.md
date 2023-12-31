42b7112ba3208875cd6d3da2135b4b544072d7e2
afmika <afmichael73@gmail.com>
2023-12-31 12:50:39 +0300

    check char sequence prefix

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # sifas-dat
-Change the host or any other metadata string for SIFAS apk client.
+Change the server host for patched SIFAS apk client.
 
 # Usage
-A common use case is to change/overwrite the server host.
-
 ```bash
 # disassemble the patched client
 java -jar apktool.jar d my-sifas-client.apk
 
 # change the host
-sifas-dat my-sifas-client/assets/bin/Data/Managed/Metadata/global-metadata.dat 192.168.1.123 192.168.1.170
+sifas-dat my-sifas-client/assets/bin/Data/Managed/Metadata/global-metadata.dat http://192.168.1.123 http://192.168.1.170
 
 # replace global-metadata.dat with output.dat (make sure to rename it)
 # reassemble
--- sifas-dat.cpp
+++ sifas-dat.cpp
@@ -28,14 +28,17 @@ int main(int argc, const char *argv[]) {
     exit(1);
   }
 
+  std::string prefix = "member_detail_badge_typeis_set\\ux4";
+
   // find the char sequence
   std::string str((std::istreambuf_iterator<char>(inp)),
                   std::istreambuf_iterator<char>());
 
-  size_t pos = str.find(searchval);
+  size_t pos = str.find(prefix + searchval);
+  size_t real_pos = pos + prefix.size();
 
   if (pos != std::string::npos) {
-    std::cout << searchval << " found at offset " << int(pos) << std::endl;
+    std::cout << searchval << " found at offset " << int(real_pos) << std::endl;
     if (replval.size() > 0) {
       std::ofstream dst("output.dat", std::ios::binary);
       std::cout << "Prepare output.dat " << std::endl;
@@ -43,7 +46,7 @@ int main(int argc, const char *argv[]) {
       inp.seekg(0, std::ios::beg);
       dst << inp.rdbuf();
 
-      dst.seekp(pos);
+      dst.seekp(real_pos);
       std::cout << "Inject value " << replval << std::endl;
       dst.write(replval.c_str(), replval.size());
 
